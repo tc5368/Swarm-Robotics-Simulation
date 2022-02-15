a5c0f3bf9eca5e99e45f0ea885ea511a27b326d0
tc5368 <tc536890@gmail.com>
2022-02-15 16:57:18 +0000

    Working to add random movement to robots

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ from mesa import Agent
 class Robot(Agent):
 	#This is the robot agent that moves around the grid fufilling customer orders.
 
-	def __init__(self, unique_id, model, y, x):
+	def __init__(self, unique_id, model, y, x, gridInfo):
 		super().__init__(unique_id, model)
 
 		#Unique ID
@@ -18,3 +18,26 @@ class Robot(Agent):
 		self.current_job = self.random.choice(['Collecting','Moving'])
 		#Queue containing the tasks assigned to the robot in some implementations.
 		self.tasks = []
+
+		self.warehouseMaxY = gridInfo[0]-1
+		self.warehouseMaxX = gridInfo[1]-1
+
+
+	def step(self):
+		print(self.x,self.y)
+
+		toMove = [self.x,self.y]
+		if self.random.choice([True,False]):
+			self.x += self.random.randint(-1,1)
+		else:
+			self.y += self.random.randint(-1,1)
+
+		print(self.x,self.y)
+
+		if 0 == self.x > self.warehouseMaxX or 0 == self.y > self.warehouseMaxY:
+			print('triggers')
+			self.model.grid.remove_agent(self)
+
+		self.model.grid.move_agent(self,(self.x,self.y))
+		#print('I am bot %s and I am at x coord: %s' %(self.unique_id,self.x))
+
--- model.py
+++ model.py
@@ -7,6 +7,8 @@ class WarehouseModel(Model):
 	#This is the warehouse model works as the base controller to creat all of the robots
 
 	def __init__(self, robotCount, width, height):
+		#Allows the model to continue to run.
+		self.running = True
 		#Number of robots in the warehouse
 		self.num_agents = robotCount
 		#The matrix that that is the warehouse floor.
@@ -18,7 +20,7 @@ class WarehouseModel(Model):
 		# Creating the Robots
 		for i in range(self.num_agents):
 			#Creates the robots starting at random points on the warehouse floor.
-			newRobot = Robot(i, self, y = self.random.randrange(height), x = self.random.randrange(width))
+			newRobot = Robot(i, self, y = self.random.randrange(height), x = self.random.randrange(width),gridInfo=[height,width])
 			#Adds the new robot to the scheduler
 			self.schedule.add(newRobot)
 
